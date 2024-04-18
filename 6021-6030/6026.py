a28d80dab5302714ce128d8af5e488e5f251815b
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-18 15:31:28 +0900

    6026 : [기초-값변환] 실수 2개 입력받아 합 계산하기(설명)(py)

---
+++ 6021-6030/6026.py
@@ -0,0 +1,14 @@
+# https://codeup.kr/problem.php?id=6026
+
+f1 = float(input())
+f2 = float(input())
+
+print(f1 + f2)
+
+# 정답
+# a=input()
+# b=input()
+# a=float(a)
+# b=float(b)
+# c=a+b
+# print(c)
