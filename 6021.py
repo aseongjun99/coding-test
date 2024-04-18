be4689a23ae8b46bbc77f04b53bbd63d16f70ba6
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-18 15:07:34 +0900

    6021 : [기초-입출력] 단어 1개 입력받아 나누어 출력하기(설명)(py)

---
+++ 6021.py
@@ -0,0 +1,13 @@
+# https://codeup.kr/problem.php?id=6021
+
+word = input()
+for alphabet in word:
+    print(alphabet)
+
+# 정답
+# s = input()
+# print(s[0])
+# print(s[1])
+# print(s[2])
+# print(s[3])
+# print(s[4])
