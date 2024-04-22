a9211236bf55a7fefa6f79b249395a548d17775e
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-22 14:03:31 +0900

    6045 : [기초-산술연산] 정수 3개 입력받아 합과 평균 출력하기(설명)(py)

---
+++ 6041-6050/6045.py
@@ -0,0 +1,14 @@
+# https://codeup.kr/problem.php?id=6045
+
+a, b, c = map(int,(input().split()))
+sum = a + b + c
+print(sum, format(sum/3,'.2f'))
+
+# 정답
+# a, b, c = input().split()
+# a=int(a)
+# b=int(b)
+# c=int(c)
+# hap=a+b+c
+# avg=hap/3
+# print(hap, format(avg, ".2f"))
