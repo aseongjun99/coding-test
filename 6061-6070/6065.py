5e6c5f74ada00fe1ecddc9fca5092d6682d81dca
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-22 15:28:45 +0900

    6065 : [기초-조건/선택실행구조] 정수 3개 입력받아 짝수만 출력하기(설명)(py)

---
+++ 6061-6070/6065.py
@@ -0,0 +1,26 @@
+# https://codeup.kr/problem.php?id=6065
+
+a, b, c = map(int, input().split())
+
+if a % 2 == 0:
+    print(a)
+if b % 2 == 0:
+    print(b)
+if c % 2 == 0:
+    print(c)
+
+# 정답
+# a, b, c = input().split()
+#
+# a = int(a)
+# b = int(b)
+# c = int(c)
+#
+# if a % 2 == 0:
+#     print(a)
+#
+# if b % 2 == 0:
+#     print(b)
+#
+# if c % 2 == 0:
+#     print(c)
