a332bfcfd24b4bfdf9fa562edcec1f57e0dda187
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-22 15:16:58 +0900

    6064 : [기초-3항연산] 정수 3개 입력받아 가장 작은 값 출력하기(설명)(py)

---
+++ 6061-6070/6064.py
@@ -0,0 +1,16 @@
+# https://codeup.kr/problem.php?id=6064
+
+a, b, c = map(int, input().split())
+
+print((a if a < b else b) if ((a if a < b else b) < c) else c)
+
+정답
+a, b, c = input().split()
+a = int(a)  #변수 a에 저장되어있는 값을 정수로 바꾸어 다시 변수 a에 저장
+b = int(b)
+c = int(c)
+
+d = a if a<b else b
+e = d if d<c else c
+
+print(e)
