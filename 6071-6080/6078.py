23f90da8e5a32c913629605d8954e7754b73158a
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-22 23:39:31 +0900

    6078 : [기초-종합] 원하는 문자가 입력될 때까지 반복 출력하기(py)

---
+++ 6071-6080/6078.py
@@ -0,0 +1,15 @@
+# https://codeup.kr/problem.php?id=6078
+
+while True:
+    c = input()
+    print(c)
+    if c == 'q':
+        break
+
+# 정답
+# while True:
+#      x=input()
+#      print(x)
+#      if x=='q':
+#           break
+#
