f7c6428b45f134824d5a6aeeb47aaa4176befd4e
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-22 15:43:47 +0900

    6068 : [기초-조건/선택실행구조] 점수 입력받아 평가 출력하기(설명)(py)

---
+++ 6061-6070/6068.py
@@ -0,0 +1,23 @@
+# https://codeup.kr/problem.php?id=6068
+
+score = int(input())
+if score >= 90:
+    print('A')
+elif score >= 70:
+    print('B')
+elif score >= 40:
+    print('C')
+else:
+    print('D')
+
+# 정답
+# a=int(input())
+#
+# if a>=90:
+#     print("A")
+# elif a>=70:
+#     print("B")
+# elif a>=40:
+#     print("C")
+# else:
+#     print("D")
