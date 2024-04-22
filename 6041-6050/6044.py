1d8a1eb5a4f0fb5a249a5856d4136133e0634857
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-22 13:58:01 +0900

    6044 : [기초-산술연산] 정수 2개 입력받아 자동 계산하기(py)

---
+++ 6041-6050/6044.py
@@ -0,0 +1,21 @@
+# https://codeup.kr/problem.php?id=6044
+
+a, b = map(int, input().split())
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+print(format(a / b, '.2f'))
+
+# 정답
+# a,b=input().split()
+# a=int(a)
+# b=int(b)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+# print(round(a/b,2))
