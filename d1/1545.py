89ee5529cc11a4ad7d082b34453c017045b95eac
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-24 13:22:07 +0900

    1545. 거꾸로 출력해 보아요

---
+++ d1/1545.py
@@ -0,0 +1,5 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV2gbY0qAAQBBAS0&categoryId=AV2gbY0qAAQBBAS0&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=1&pageSize=10&pageIndex=2
+
+n = int(input())
+for i in range(n, -1, -1):
+    print(i, end=' ')
