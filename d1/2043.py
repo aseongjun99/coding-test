514576c2da4b6f3a940b77f81e3d4e877cbbfc6d
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-04-24 12:00:57 +0900

    2043. 서랍의 비밀번호

---
+++ d1/2043.py
@@ -0,0 +1,4 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QJ_8KAx8DFAUq&categoryId=AV5QJ_8KAx8DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=1&pageSize=10&pageIndex=2
+
+P, K = map(int, input().split())
+print(P - K + 1)
