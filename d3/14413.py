5e7d1135ad8a95e52c8b8c51c909815d2568e309
aseongjun99@gmail.com <aseongjun99@gmail.com>
2024-05-14 16:39:06 +0900

    풀이중

--- d3/14413.py
+++ d3/14413.py
@@ -1,10 +1,10 @@
-# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AYEXgKnKKg0DFARx&categoryId=AYEXgKnKKg0DFARx&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=3&pageSize=30&pageIndex=1
-
-t = int(input()) + 1
-for tc in range(1, t):
-    n, m = map(int, input().split())
-    board = [list(input()) for i in range(n)]
-
-    for i in range(n):
-        for j in range(m):
-            # 범위 체크
+# # https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AYEXgKnKKg0DFARx&categoryId=AYEXgKnKKg0DFARx&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=3&pageSize=30&pageIndex=1
+#
+# t = int(input()) + 1
+# for tc in range(1, t):
+#     n, m = map(int, input().split())
+#     board = [list(input()) for i in range(n)]
+#
+#     for i in range(n):
+#         for j in range(m):
+#             # 범위 체크
