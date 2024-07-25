90a77b69b4528f23479b3e7ab839a693c3895733
aseongjun99 <154566676+aseongjun99@users.noreply.github.com>
2024-07-25 12:12:51 +0900

    [Silver V] Title: 단어 나누기, Time: 84 ms, Memory: 12584 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.java
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static String partialReverse(int midLength, String str) {
-		String result = "z" + str;
+		String result = "z" + str; // 초기 상태가 사전 순서 상 가장 앞서는 단어를 제외.
 		for (int i=1;i+midLength<str.length();i++) {
 			StringBuilder left = new StringBuilder(str.substring(0, i)).reverse();
 			StringBuilder mid = new StringBuilder(str.substring(i, i + midLength)).reverse();
@@ -29,7 +29,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
-		String answer = "z" + input;
+		String answer = "z" + input; // 초기 상태가 사전 순서 상 가장 앞서는 단어를 제외.
 		// 1. 가운데 알파벳의 길이를 1개, 2개, 3개, ..., n-2개 까지 설정하면서 순회
 		// 2. 순회하면서 알파벳의 사전 순서 비교.
 		int midLength = 1;
