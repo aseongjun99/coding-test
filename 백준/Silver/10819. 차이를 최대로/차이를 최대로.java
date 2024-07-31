63dce5f572457e4b3a92b18c76eca7dd2441aa18
aseongjun99 <154566676+aseongjun99@users.noreply.github.com>
2024-08-01 08:53:34 +0900

    [Silver II] Title: 차이를 최대로, Time: 232 ms, Memory: 35824 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int combination(List<Integer> input, List<Integer> comb) {
+		int max = 0;
+		if (input.size() == 0) {
+			for (int i=0;i<comb.size()-1;i++) {
+				max += Math.abs(comb.get(i) - comb.get(i+1));
+			}
+		} else {
+			for (int i=0;i<input.size();i++) {
+				List<Integer> nextComb = new ArrayList<>(comb);
+				nextComb.add(input.get(i));
+				
+				List<Integer> nextInput = new ArrayList<>(input);
+				nextInput.remove(input.indexOf(input.get(i)));
+				
+				max = Math.max(combination(nextInput, nextComb), max);
+			}	
+		}
+		return max;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		List<Integer> A = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i=0;i<N;i++) {
+			A.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int answer = combination(A, new ArrayList<>());
+		System.out.println(answer);
+	}
+}
