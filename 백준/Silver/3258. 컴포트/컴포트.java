8e5297f22a82052dc390ae8b4f4d27b8a80aea17
aseongjun99 <154566676+aseongjun99@users.noreply.github.com>
2024-07-31 17:19:18 +0900

    [Silver III] Title: 컴포트, Time: 84 ms, Memory: 13100 KB -BaekjoonHub

---
+++ 백준/Silver/3258. 컴포트/컴포트.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int Z = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[] obstaclePos = new int[M];
+		for (int i=0;i<M;i++) {
+			obstaclePos[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int pos = 1;
+		int K = 1;
+		int[] visit = new int[N+1];
+		while (true) {
+			if ((pos + K) > N) {
+				pos = (pos + K) % N;
+			} else {
+				pos = (pos + K);
+			}
+			
+			if (pos == Z) {
+				System.out.println(K);
+				return;
+			} else if (visit[pos] == 1) {
+				pos = 1;
+				K++;
+				visit = new int[N+1];
+			} else {
+				for (int i=0;i<M;i++) {
+					if (obstaclePos[i] == pos) {
+						pos = 1;
+						K++;
+						visit = new int[N+1];
+						break;
+					}
+				}
+				visit[pos] =1;
+			}
+		}
+	}
+}
