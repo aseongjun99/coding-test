9b56c2e32c7284df0de1b2eda2740897c82bd4d9
aseongjun99 <154566676+aseongjun99@users.noreply.github.com>
2024-08-06 15:52:16 +0900

    [Silver I] Title: 부등호, Time: 1212 ms, Memory: 538532 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -9,18 +9,18 @@ public class Main {
 	
 	static List<Integer> nums = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9)); 
 	
-	static void combination(int k, List<Integer> comb, List<List<Integer>> result) {
-		if (comb.size() == k+1) {
-			result.add(new ArrayList<>(comb));
-		} else if (comb.size() < k+1) {
+	static void permutation(int k, List<Integer> perm, List<List<Integer>> result) {
+		if (perm.size() == k+1) {
+			result.add(new ArrayList<>(perm));
+		} else if (perm.size() < k+1) {
 			for (int i=0;i<nums.size();i++) {
 				int n = nums.get(i);
-				comb.add(n);
+				perm.add(n);
 				nums.remove(i);
 				
-				combination(k, comb, result);
+				permutation(k, perm, result);
 				
-				comb.remove(comb.size()-1);
+				perm.remove(perm.size()-1);
 				nums.add(i, n);
 			}
 		}
@@ -41,12 +41,12 @@ public class Main {
 		return true;
 	}
 	
-	static String findMin(String[] inequality, List<List<Integer>> comb) {
+	static String findMin(String[] inequality, List<List<Integer>> perm) {
 		StringBuilder min = new StringBuilder();
-		for (int i=0;i<comb.size();i++) {
-			if (isAvailable(comb.get(i), inequality)) {
-				for (int j=0;j<comb.get(i).size();j++) {
-					min.append(comb.get(i).get(j));
+		for (int i=0;i<perm.size();i++) {
+			if (isAvailable(perm.get(i), inequality)) {
+				for (int j=0;j<perm.get(i).size();j++) {
+					min.append(perm.get(i).get(j));
 				}
 				break;
 			}
@@ -54,12 +54,12 @@ public class Main {
 		return min.toString();
 	}
 	
-	static String findMax(String[] inequality, List<List<Integer>> comb) {
+	static String findMax(String[] inequality, List<List<Integer>> perm) {
 		StringBuilder max = new StringBuilder();
-		for (int i=comb.size()-1;i>-1;i--) {
-			if (isAvailable(comb.get(i), inequality)) {
-				for (int j=0;j<comb.get(i).size();j++) {
-					max.append(comb.get(i).get(j));
+		for (int i=perm.size()-1;i>-1;i--) {
+			if (isAvailable(perm.get(i), inequality)) {
+				for (int j=0;j<perm.get(i).size();j++) {
+					max.append(perm.get(i).get(j));
 				}
 				break;
 			}
@@ -72,10 +72,10 @@ public class Main {
 		int k = Integer.parseInt(br.readLine());
 		String[] inequality = br.readLine().split(" ");
 		
-		List<List<Integer>> comb= new ArrayList<>(); 
-		combination(k, new ArrayList<>(), comb);
+		List<List<Integer>> perm= new ArrayList<>(); 
+		permutation(k, new ArrayList<>(), perm);
 
-		System.out.println(findMax(inequality, comb));
-		System.out.println(findMin(inequality, comb));
+		System.out.println(findMax(inequality, perm));
+		System.out.println(findMin(inequality, perm));
 	}
 }
