a030ed5c5028a9d6069274a1f3a91252d32d4329
aseongjun99 <154566676+aseongjun99@users.noreply.github.com>
2024-10-19 20:38:55 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.09 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -2,13 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        // t초 동안 1초마다 x 만큼 회복
-        // t초 연속 감기 성공 -> y 추가 회복
-        // 최대 체력 초과 불가
-        // 피격 시 취소
-        // 피격 도중 체력 회복 불가
-        // 취소 또는 종료 시 즉시 다시 사용 -> 연속 성공 시간 0으로 초기화
-        // 체력 0 이하 되면 체력 회복 불가
         int maxHealth = health;
         for (int i=0;i<attacks.length-1;i++) {
             health -= attacks[i][1];
